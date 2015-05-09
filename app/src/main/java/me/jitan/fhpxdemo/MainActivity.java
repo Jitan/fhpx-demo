8b9499fcc0e1329f85bef96778dfcdeb260ef1ed
Jitan Elis <jitan.elis@gmail.com>
2015-05-09 10:53:36 +0200

    Shows/Hides keyboard properly when searching

--- app/src/main/java/me/jitan/fhpxdemo/MainActivity.java
+++ app/src/main/java/me/jitan/fhpxdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package me.jitan.fhpxdemo;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
@@ -7,12 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -47,6 +52,9 @@ public class MainActivity extends AppCompatActivity
         mIconCloseSearch = getResources().getDrawable(R.drawable.ic_close);
         mIconOpenSearch = getResources().getDrawable(R.drawable.ic_search);
         mSearchWatcher = new SearchWatcher();
+        mSearchField = (EditText) findViewById(R.id.etSearch);
+        mSearchField.addTextChangedListener(mSearchWatcher);
+        setKeyboardSearchListener();
 
 
         // Gridview
@@ -92,32 +100,26 @@ public class MainActivity extends AppCompatActivity
                 if (mSearchOpened)
                 {
                     closeSearchBar();
+                    hideKeyboard();
                 }
                 else
                 {
                     openSearchBar(mSearchQuery);
+                    showKeyboard();
                 }
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void openSearchBar(String queryText)
+    public void hideKeyboard()
     {
-
-        // Set custom view on action bar.
-        if (mActionBar != null)
+        View view = getCurrentFocus();
+        if (view != null)
         {
-            // Search edit text field setup.
-            mSearchField = (EditText) findViewById(R.id.etSearch);
-            mSearchField.setVisibility(View.VISIBLE);
-            mSearchField.addTextChangedListener(mSearchWatcher);
-            mSearchField.setText(queryText);
-            mSearchField.requestFocus();
-
-            // Change search icon accordingly.
-            mSearchAction.setIcon(mIconCloseSearch);
-            mSearchOpened = true;
+            InputMethodManager manager = (InputMethodManager) getSystemService(
+                    Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
 
@@ -132,11 +134,34 @@ public class MainActivity extends AppCompatActivity
         // Change search icon accordingly.
         mSearchAction.setIcon(mIconOpenSearch);
         mSearchOpened = false;
-
     }
 
+    private void openSearchBar(String queryText)
+    {
+        // Set custom view on action bar.
+        if (mActionBar != null)
+        {
+            // Search edit text field setup.
+            mSearchField.setVisibility(View.VISIBLE);
+            mSearchField.setText(queryText);
+            mSearchField.requestFocus();
+
+            // Change search icon accordingly.
+            mSearchAction.setIcon(mIconCloseSearch);
+            mSearchOpened = true;
+        }
+    }
+
+    private void showKeyboard()
+    {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        // only will trigger it if no physical keyboard is open
+        inputMethodManager.showSoftInput(mSearchField, InputMethodManager.SHOW_IMPLICIT);
+    }
     private class SearchWatcher implements TextWatcher
     {
+
         @Override
         public void beforeTextChanged(CharSequence c, int i, int i2, int i3)
         {
@@ -148,13 +173,31 @@ public class MainActivity extends AppCompatActivity
         {
 
         }
-
         @Override
         public void afterTextChanged(Editable editable)
         {
             mSearchQuery = mSearchField.getText().toString();
-            Toast.makeText(getApplicationContext(), mSearchQuery, Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private void setKeyboardSearchListener()
+    {
+        mSearchField.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+            {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH)
+                {
+                    mSearchQuery = mSearchField.getText().toString();
+                    Toast.makeText(getApplicationContext(), "Searching for: " + mSearchQuery,
+                            Toast.LENGTH_SHORT).show();
+                    hideKeyboard();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
